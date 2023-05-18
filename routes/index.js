89eb22bdb3706b6a89019eb645923dade9d4ba85
ColeC <contactcolecarter@gmail.com>
2023-05-18 14:56:38 -0400

    wrote a functioning route

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+// localhost:3001
+router.get('/', (req, res) => {
+    res.json('HI!');
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const routes = require('./routes');
 const PORT = 3001;
 const app = express();
 
+app.use(routes);
 
 app.listen(PORT, () => {
-    console.log('sick');
+    console.log('this is my console log, there are many others like it, but this one is mine.');
 });
