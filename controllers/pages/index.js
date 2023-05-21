1224b4b63cd2dc5292f91b8ae78a7c1ab05a5bc7
ColeC <contactcolecarter@gmail.com>
2023-05-21 19:07:45 -0400

    got the user data to display on the main page

--- controllers/pages/index.js
+++ controllers/pages/index.js
@@ -1,8 +1,18 @@
 // made for the sake of learning - Jung 2023 timestamp 01:01:09
 const router = require('express').Router();
 const { User } = require('../../models');
-router.get('/', (req, res) => {
-    res.render('homepage');
+router.get('/', async (req, res) => {
+    const userData = await User.findAll({
+        attributes: [
+            'id',
+            'username'
+        ]
+    });
+    console.log("UGLY DATA BOO", userData);
+
+    const prettyUserData = userData.map((user) => user.get({plain: true}));
+    console.log('PRETTY USER DATA', prettyUserData);
+    res.render('homepage', {prettyUserData});
 });
 
 // localhost:3001/dashboard
